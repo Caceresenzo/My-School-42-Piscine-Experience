ac53b72fc21cb9cafe6c2d1e6fd2a22764232778
Caceresenzo <caceresenzo1502@gmail.com>
2019-08-07 19:17:21 +0200

    day07: 2nd try [complete] [5%]

--- day07: C Piscine C 05/ex01/ft_recursive_factorial_dev.c
+++ day07: C Piscine C 05/ex01/ft_recursive_factorial_dev.c
@@ -12,19 +12,19 @@
 
 #include <stdio.h>
 
-int	ft_iterative_factorial_recursive(int factorial, int number)
+int	ft_recursive_factorial_recursive(int factorial, int number)
 {
 	factorial *= number--;
 	if (number > 0)
-		return (ft_iterative_factorial_recursive(factorial, number));
+		return (ft_recursive_factorial_recursive(factorial, number));
 	return (factorial);
 }
 
-int	ft_iterative_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	return (ft_iterative_factorial_recursive(1, nb));
+	return (ft_recursive_factorial_recursive(1, nb));
 }
 
 int	main(void)
@@ -34,7 +34,7 @@ int	main(void)
 	n = -2;
 	while (n < 7)
 	{
-		printf("fact(%d) = %d\n", n, ft_iterative_factorial(n));
+		printf("fact(%d) = %d\n", n, ft_recursive_factorial(n));
 		n++;
 	}
 }
