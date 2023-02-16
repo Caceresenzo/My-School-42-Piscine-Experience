2237acdb4043cec5f37391c9154d1c731a3ff802
Tyrfing1031 <99285296+Tyrfing1031@users.noreply.github.com>
2023-02-17 00:44:36 +0900

    Update add_chelou.sh

--- day01: C Piscine Shell 01/ex08/add_chelou.sh
+++ day01: C Piscine Shell 01/ex08/add_chelou.sh
@@ -1,5 +1,2 @@
-echo $FT_NBR1 + $FT_NBR2 \
-  | tr "'"'\\"?!mrdoc' '0123401234' \
-  | xargs -I{} echo 'ibase=5;obase=23;'{} \
-  | bc \
-  | tr '0123456789ABC' 'gtaio luSnemf'
+#!/bin/bash
+echo $FT_NBR1 + $FT_NBR2 | tr "\'" "0" | tr "\" "1" | tr "\"" "2" | tr "\?" "3" | tr "!" "4" | tr "mrdoc" "01234" | xargs echo "obase=13;ibase=5;" | bc | tr "0123456789ABC" "gtaio luSnemf"
